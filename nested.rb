19e442d3bde5d1d05ff97be5b545934704f9ecc9
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-23 01:06:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,8 +82,8 @@ def adding_matz
       }
     }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
-   :languages => ["LISP", "C"]
-}
+   :languages => ["LISP", "C"]}
+puts programmer_hash
 end
 
 def changing_alan
